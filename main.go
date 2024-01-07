a5209081f4a19f5d0f0cf62c10c796eab7d0cffe
Jeffrey Smith <jeffrey.smith7@gmail.com>
2024-01-07 07:06:47 -0500

    Added stdin parsing. Need to add byte vs utf8 char len

--- main.go
+++ main.go
@@ -1,13 +1,79 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
-
+	"strings"
+	"unicode/utf8"
 )
 
+type Line struct {
+	text string
+	words,bytes,chars int
+}
+func initLine(text string)Line{
+	line := Line{text:text}
+	line.words = len(strings.Fields(line.text))
+	line.bytes = len(line.text)
+	line.chars = utf8.RuneCountInString(line.text)
+	return line
+}
+func parseLines(Lines []Line) (int,int,int){
+	
+	wordCount := 0
+	byteCount := 0
+	charCount := 0
+
+	for _, l := range Lines{
+		wordCount+=l.words
+		byteCount+=l.bytes
+		charCount+=l.chars
+	}
+
+	return wordCount,byteCount,charCount
+}
+func parseStdin()([]Line,int,int){ 
+	var Lines []Line
+
+	lineCount := 0
+	longest := 0
+
+	scanner := bufio.NewScanner(os.Stdin)
+	line := ""
+
+	scanner.Split(bufio.ScanRunes)
+	for scanner.Scan(){
+		if scanner.Text() == "\n"{
+			if len(strings.Trim(line,"\n")) > longest{
+				longest = len(strings.Trim(line,"\n"))
+			}
+			newLine := initLine(line)
+			Lines = append(Lines,newLine)
+			line = ""
+			lineCount++
+		}
+		line += scanner.Text()
+	}
+	//Deal with no new line at the end of input
+	if len(Lines) == 0 || len(line) > 0 {
+		if len(line) > longest {
+			longest = len(line)
+		}
+		newLine := initLine(line)
+		Lines = append(Lines,newLine)
+	}
+	return Lines,lineCount,longest
+}
+
+
 func main(){
+	var Lines []Line
+	var count int
+	var longest int
+
+	
 	word := flag.Bool("w",false,"Display the number of words")
 	bytes := flag.Bool("c",false,"Display the number of bytes")
 	lineCount := flag.Bool("l",false,"Display the number of lines")
@@ -30,11 +96,17 @@ func main(){
 	
 	if len(fileNames) == 0 && info.Mode() & os.ModeCharDevice == 0{
 		//Process stdin stuff
+		Lines,count,longest = parseStdin()
+		
 	} else if len(fileNames) == 0 {
-		//Run until EOF hit
+		//Run until EOF hit ()
 	} else {
 		//Process all files passed in
 	}
-	
+
+	//Process output here
+	w,b,c := parseLines(Lines)
+	fmt.Println(w,b,c)
+	fmt.Println(count,longest)
 	fmt.Println(*word,*bytes,*lineCount,*chars)
 }
